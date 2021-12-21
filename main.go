0258d19b3446b4bd1c6b6ac34986718226f11081
Jakkrit Sangakong <my.phoenix168@gmail.com>
2021-12-21 20:36:48 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+	"saraban/routes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	r := gin.Default()
+
+	r.Static("/uploads", "./uploads")
+
+	uploadDirs := [...]string{"articles", "users"}
+	for _, dir := range uploadDirs {
+		os.MkdirAll("uploads/"+dir, 0755)
+	}
+	routes.Serve(r)
+
+	r.Run(":" + os.Getenv("PORT"))
+}
